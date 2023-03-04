34429336a19969773ad845884088696772af73a0
Hanna <theannalios@gmail.com>
2023-03-04 20:06:19 +0300

    URL's of microservices moved to MicroserviceProperty

--- src/main/java/com/solvd/review/service/impl/ReviewServiceImpl.java
+++ src/main/java/com/solvd/review/service/impl/ReviewServiceImpl.java
@@ -5,6 +5,7 @@ import com.solvd.review.domain.criteria.SearchCriteria;
 import com.solvd.review.domain.exception.ResourceNotFoundException;
 import com.solvd.review.persistence.ReviewRepository;
 import com.solvd.review.service.ReviewService;
+import com.solvd.review.service.property.MicroserviceProperty;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -17,23 +18,24 @@ import java.util.Objects;
 @RequiredArgsConstructor
 public class ReviewServiceImpl implements ReviewService {
 
-    private static final String MOVIES_URL = "http://movie/api/v1/movies";
     private final ReviewRepository reviewRepository;
+    private final MicroserviceProperty microserviceProperty;
     private final RestTemplate restTemplate;
 
     @Override
     public List<Review> retrieveByCriteria(SearchCriteria searchCriteria) {
-        return Objects.nonNull(searchCriteria.getMovieId()) ?
+        return Objects.nonNull(searchCriteria) && Objects.nonNull(searchCriteria.getMovieId()) ?
                 reviewRepository.findByMovieId(searchCriteria.getMovieId()) :
                 reviewRepository.findAll();
     }
 
     @Override
     public Review create(Review review) {
-        Boolean isExists = restTemplate.getForObject(MOVIES_URL + "/exists/" + review.getMovieId(),
+        Boolean isExists = restTemplate.getForObject(
+                microserviceProperty.getMovieUrl() + "/exists/" + review.getMovieId(),
                 Boolean.class);
         if (Boolean.FALSE.equals(isExists)) {
-            throw new ResourceNotFoundException("Movie with id = {} doesn't exist!" + review.getMovieId());
+            throw new ResourceNotFoundException("Movie with id = " + review.getMovieId() + " doesn't exist!");
         }
         review.setDate(LocalDate.now());
         return reviewRepository.save(review);
