835118766aab6394b5219a56e8211b2c0e0e1a82
Hanna <theannalios@gmail.com>
2023-03-09 14:07:08 +0300

    Bugs are fixed, MicroserviceProperty is removed

--- src/main/java/com/solvd/review/service/impl/ReviewServiceImpl.java
+++ src/main/java/com/solvd/review/service/impl/ReviewServiceImpl.java
@@ -5,7 +5,6 @@ import com.solvd.review.domain.criteria.SearchCriteria;
 import com.solvd.review.domain.exception.ResourceNotFoundException;
 import com.solvd.review.persistence.ReviewRepository;
 import com.solvd.review.service.ReviewService;
-import com.solvd.review.service.property.MicroserviceProperty;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -21,9 +20,9 @@ import java.util.Objects;
 @RequiredArgsConstructor
 public class ReviewServiceImpl implements ReviewService {
 
+    private final static String MOVIE_URL = "http://movie/api/v1/movies";
     private final ReviewRepository reviewRepository;
     private final WebClient.Builder webClientBuilder;
-    private final MicroserviceProperty microserviceProperty;
 
     @Override
     public Flux<Review> retrieveByCriteria(SearchCriteria searchCriteria) {
@@ -36,10 +35,9 @@ public class ReviewServiceImpl implements ReviewService {
     public Mono<Review> create(Review review) {
         Mono<Boolean> exists = webClientBuilder.build()
                 .get()
-                .uri(microserviceProperty.getMovieUrl() + "/exists/{movieId}", review.getMovieId())
+                .uri(MOVIE_URL + "/exists/{movieId}", review.getMovieId())
                 .retrieve()
                 .bodyToMono(Boolean.class);
-
         return exists.flatMap(value -> {
             if (Boolean.FALSE.equals(value)) {
                 return Mono.error(new ResourceNotFoundException("Movie with id = " + review.getMovieId() + " doesn't exist!"));
