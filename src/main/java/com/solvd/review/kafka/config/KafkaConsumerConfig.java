e6b33570cdd81e7af3228044d1a3cb9ab208f2e4
Hanna <theannalios@gmail.com>
2023-03-14 15:21:07 +0300

    XmlParser for XML data is implemented, resolved #8

--- src/main/java/com/solvd/review/kafka/config/KafkaConsumerConfig.java
+++ src/main/java/com/solvd/review/kafka/config/KafkaConsumerConfig.java
@@ -1,10 +1,9 @@
 package com.solvd.review.kafka.config;
 
+import com.solvd.review.kafka.parser.XmlParser;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.LongDeserializer;
-import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.kafka.receiver.KafkaReceiver;
@@ -19,20 +18,16 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class KafkaConsumerConfig {
 
-    private static final String BOOTSTRAP_SERVERS = "localhost:9092";
-    private static final String GROUP_ID = "group";
-    private static final String TOPIC = "movies";
-
     @Bean
     public ReceiverOptions<String, Long> receiverOptions() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class);
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, XmlParser.getValue("bootstrapServers"));
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, XmlParser.getValue("groupId"));
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, XmlParser.getValue("keyDeserializer"));
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,  XmlParser.getValue("valueDeserializer"));
         ReceiverOptions<String, Long> receiverOptions = ReceiverOptions.create(props);
         return receiverOptions
-                .subscription(Collections.singleton(TOPIC))
+                .subscription(Collections.singleton( XmlParser.getValue("topic")))
                 .addAssignListener(receiverPartitions -> log.debug("Assigned: " + receiverPartitions))
                 .addRevokeListener(receiverPartitions -> log.debug("Revoked: " + receiverPartitions));
     }
